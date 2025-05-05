8bdbfd02dc882f90500b0f6bc95b4f5154e196e9
mbali-18 <faithfulservent18@gmail.com>
2025-05-05 14:33:25 +0200

    first commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yi41WHmkTaFawNiUIvDIyGhzznOJlJKjhZ6+V3ahjFo=\"\n}"
---
+++ app/news/pages.js
@@ -0,0 +1,22 @@
+import Link from next
+
+export default function NewsPage() {
+  return (
+    
+    <>
+    <h1>News Page</h1>
+    <ul>
+        <li>
+            <Link href="news/first-news">First News Item</Link>
+        </li>
+        <li>
+            <Link href="news/second-news">Second News Item</Link>
+        </li>
+        <li>
+            <Link href="news/third-news">Third News Item</Link>
+        </li>
+        </ul>
+    </>
+  )
+  
+}
